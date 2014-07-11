1b4a9a2d2a652a84bdffbf9c1209ef41beec2f96
krk324 <krk32488@gmail.com>
2014-07-11 09:18:11 -0400

    Add access token to the insagram api.

--- app/models/insta.rb
+++ app/models/insta.rb
@@ -1,16 +1,18 @@
 class Insta
 
   def self.images(latitude,longitude)
+    client = Instagram.client(access_token: ENV['ACCESS_TOKEN'])
     html = "<h2>Recent Insagram Photo</h2>"
-    for media_item in Instagram.client.media_search(latitude,longitude)
+    for media_item in client.media_search(latitude,longitude)
       html << "<img src='#{media_item.images.thumbnail.url}'>"
     end
     html
   end
 
   def self.popular_images
+    client = Instagram.client(access_token: ENV['ACCESS_TOKEN'])
     html = "<h1>Trending Photos</h1>"
-    for media_item in Instagram.client.media_popular
+    for media_item in client.media_popular
       html << "<img src='#{media_item.images.thumbnail.url}'>"
     end
     html
