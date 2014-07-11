4e865983af5ef74689f76bcfecea435438d6e42e
krk324 <krk32488@gmail.com>
2014-07-10 23:52:49 -0400

    Insagram api fix

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,8 @@ Rails.application.initialize!
 
 # Set user name to GeoNamesAPI
 GeoNamesAPI.username = ENV['USER_NAME']
+
+Instagram.configure do |config|
+  config.client_id = ENV["CLIENT_ID"]
+  config.client_secret = ENV["CLIENT_SECRET"]
+end
--- config/initializers/instagram.rb
+++
@@ -1,4 +0,0 @@
-Instagram.configure do |config|
-  config.client_id = ENV["CLIENT_ID"]
-  config.client_secret = ENV["CLIENT_SECRET"]
-end
